7bb6e877cc849b767db3cea7483d3e4ae9e3fbd5
Ray Johnson <rjohnson@yp.com>
2013-05-31 17:07:26 -0700

    4th try

--- lib/zuora/objects/product_rate_plan.rb
+++ lib/zuora/objects/product_rate_plan.rb
@@ -2,7 +2,6 @@ module Zuora::Objects
   class ProductRatePlan < Base
     belongs_to :product
     has_many :product_rate_plan_charges
-    has_many :active_currencies
 
     validates_length_of :description, :maximum => 500, :allow_nil => true
     validates_datetime_of :effective_start_date, :effective_end_date
@@ -11,7 +10,7 @@ module Zuora::Objects
     define_attributes do
       read_only :updated_by_id, :updated_date, :created_by_id, :created_date
 
-      complex :active_currencies
+      write_only :active_currencies
     end
   end
 end
